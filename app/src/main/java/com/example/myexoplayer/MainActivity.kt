0a46ab90cfd7106cc58a95e89eaf93d9ce7c68c1
Fauzan Akmal Mahdi <recovery252@gmail.com>
2024-06-23 23:18:22 +0700

    update acti main

--- app/src/main/java/com/example/myexoplayer/MainActivity.kt
+++ app/src/main/java/com/example/myexoplayer/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.myexoplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.media3.common.MediaItem
+import androidx.media3.exoplayer.ExoPlayer
+import com.example.myexoplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val videoItem =
+            MediaItem.fromUri("https://github.com/dicodingacademy/assets/releases/download/release-video/VideoDicoding.mp4")
+
+        val player = ExoPlayer.Builder(this).build().also { exoPlayer ->
+            exoPlayer.setMediaItem(videoItem)
+            exoPlayer.prepare()
+        }
+
+        binding.playerView.player = player
     }
 }
